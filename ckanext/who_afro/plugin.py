fd3be083c8e9e763876e3b19c5ada07dbe2b835a
Kacper Krupinski <kacper.krupinski@gmail.com>
2024-06-05 08:33:07 +0200

    WAC-42: Check type before loading to dict.

--- ckanext/who_afro/plugin.py
+++ ckanext/who_afro/plugin.py
@@ -30,6 +30,7 @@ class WHOAFROPlugin(plugins.SingletonPlugin, DefaultTranslation):
     # ITemplateHelpers
     def get_helpers(self):
         return {
+<<<<<<< Updated upstream
             'max_resource_size': uploader.get_max_resource_size,
             'get_dataset_from_id': who_afro_helpers.get_dataset_from_id,
             'blob_storage_resource_filename': blobstorage_helpers.resource_filename,
@@ -43,6 +44,20 @@ class WHOAFROPlugin(plugins.SingletonPlugin, DefaultTranslation):
             'get_last_modifier': who_afro_helpers.get_last_modifier,
             'format_locale': who_afro_helpers.format_locale,
             'get_datahub_stats': who_afro_helpers.get_datahub_stats
+=======
+            "max_resource_size": uploader.get_max_resource_size,
+            "get_dataset_from_id": who_afro_helpers.get_dataset_from_id,
+            "blob_storage_resource_filename": blobstorage_helpers.resource_filename,
+            "get_facet_items_dict": who_afro_helpers.get_facet_items_dict,
+            "get_all_groups": who_afro_helpers.get_all_groups,
+            "get_featured_datasets": who_afro_helpers.get_featured_datasets,
+            "get_user_from_id": who_afro_helpers.get_user_from_id,
+            "get_user_obj": who_afro_helpers.get_user_obj,
+            "month_formatter": who_afro_helpers.month_formatter,
+            "get_recently_updated_datasets": who_afro_helpers.get_recently_updated_datasets,
+            "get_last_modifier": who_afro_helpers.get_last_modifier,
+            "format_locale": who_afro_helpers.format_locale,
+>>>>>>> Stashed changes
         }
 
     # IConfigurer
@@ -54,30 +69,30 @@ class WHOAFROPlugin(plugins.SingletonPlugin, DefaultTranslation):
     # IFacets
     def dataset_facets(self, facet_dict, package_type):
         new_facet_dict = OrderedDict()
-        new_facet_dict['groups'] = facet_dict['groups']
-        new_facet_dict['programme'] = plugins.toolkit._('Programmes')
-        new_facet_dict['country'] = plugins.toolkit._('Countries')
-        new_facet_dict['tags'] = plugins.toolkit._('Keywords')
-        new_facet_dict['res_format'] = plugins.toolkit._('Formats')
-        new_facet_dict['organization'] = facet_dict['organization']
+        new_facet_dict["groups"] = facet_dict["groups"]
+        new_facet_dict["programme"] = plugins.toolkit._("Programmes")
+        new_facet_dict["country"] = plugins.toolkit._("Countries")
+        new_facet_dict["tags"] = plugins.toolkit._("Keywords")
+        new_facet_dict["res_format"] = plugins.toolkit._("Formats")
+        new_facet_dict["organization"] = facet_dict["organization"]
         return new_facet_dict
 
     def group_facets(self, facet_dict, group_type, package_type):
         new_facet_dict = OrderedDict()
-        new_facet_dict['programme'] = plugins.toolkit._('Programmes')
-        new_facet_dict['country'] = plugins.toolkit._('Countries')
-        new_facet_dict['tags'] = plugins.toolkit._('Keywords')
-        new_facet_dict['res_format'] = plugins.toolkit._('Formats')
-        new_facet_dict['organization'] = facet_dict['organization']
+        new_facet_dict["programme"] = plugins.toolkit._("Programmes")
+        new_facet_dict["country"] = plugins.toolkit._("Countries")
+        new_facet_dict["tags"] = plugins.toolkit._("Keywords")
+        new_facet_dict["res_format"] = plugins.toolkit._("Formats")
+        new_facet_dict["organization"] = facet_dict["organization"]
         return new_facet_dict
 
     def organization_facets(self, facet_dict, organization_type, package_type):
         new_facet_dict = OrderedDict()
-        new_facet_dict['groups'] = facet_dict['groups']
-        new_facet_dict['programme'] = plugins.toolkit._('Programmes')
-        new_facet_dict['country'] = plugins.toolkit._('Countries')
-        new_facet_dict['tags'] = plugins.toolkit._('Keywords')
-        new_facet_dict['res_format'] = plugins.toolkit._('Formats')
+        new_facet_dict["groups"] = facet_dict["groups"]
+        new_facet_dict["programme"] = plugins.toolkit._("Programmes")
+        new_facet_dict["country"] = plugins.toolkit._("Countries")
+        new_facet_dict["tags"] = plugins.toolkit._("Keywords")
+        new_facet_dict["res_format"] = plugins.toolkit._("Formats")
         return new_facet_dict
 
     # IResourceController
@@ -92,45 +107,49 @@ class WHOAFROPlugin(plugins.SingletonPlugin, DefaultTranslation):
     # IActions
     def get_actions(self):
         return {
-            'user_list': who_afro_actions.user_list,
-            'dataset_duplicate': who_afro_actions.dataset_duplicate,
-            'package_create': who_afro_actions.package_create,
-            'dataset_tag_replace': who_afro_actions.dataset_tag_replace,
-            'user_show_me': who_afro_actions.user_show_me,
+            "user_list": who_afro_actions.user_list,
+            "dataset_duplicate": who_afro_actions.dataset_duplicate,
+            "package_create": who_afro_actions.package_create,
+            "dataset_tag_replace": who_afro_actions.dataset_tag_replace,
+            "user_show_me": who_afro_actions.user_show_me,
         }
 
     # IValidators
     def get_validators(self):
         return {
-            'autogenerate_name_from_title': who_afro_validators.autogenerate_name_from_title,
-            'autofill': who_afro_validators.autofill,
-            'autogenerate': who_afro_validators.autogenerate,
-            'isomonth': who_afro_validators.isomonth
+            "autogenerate_name_from_title": who_afro_validators.autogenerate_name_from_title,
+            "autofill": who_afro_validators.autofill,
+            "autogenerate": who_afro_validators.autogenerate,
+            "isomonth": who_afro_validators.isomonth,
         }
 
     # IPackageContoller
     def after_dataset_delete(self, context, data_dict):
-        package_data = toolkit.get_action('package_show')(context, data_dict)
-        if package_data.get('private'):
-            package_data['state'] = 'deleted'
-            context['package'].state = 'deleted'
+        package_data = toolkit.get_action("package_show")(context, data_dict)
+        if package_data.get("private"):
+            package_data["state"] = "deleted"
+            context["package"].state = "deleted"
             who_afro_upload.add_activity(context, package_data, "changed")
 
     def after_dataset_update(self, context, data_dict):
-        if data_dict.get('private'):
+        if data_dict.get("private"):
             who_afro_upload.add_activity(context, data_dict, "changed")
 
     def after_dataset_create(self, context, data_dict):
-        if data_dict.get('private'):
+        if data_dict.get("private"):
             who_afro_upload.add_activity(context, data_dict, "new")
 
     def before_dataset_index(self, data_dict):
-        data_dict['programme'] = json.loads(data_dict['programme'])
-        data_dict['country'] = json.loads(data_dict['country'])
+        if isinstance(data_dict["programme"], str):
+            data_dict["programme"] = json.loads(data_dict["programme"])
+        if isinstance(data_dict["country"], str):
+            data_dict["country"] = json.loads(data_dict["country"])
         return data_dict
 
     def get_blueprint(self):
-        log.info(f"Registering the following blueprints: {who_afro_blueprints.get_blueprints()}")
+        log.info(
+            f"Registering the following blueprints: {who_afro_blueprints.get_blueprints()}"
+        )
         return who_afro_blueprints.get_blueprints()
 
     # ITranslation
