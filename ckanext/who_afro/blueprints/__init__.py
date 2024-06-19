24b20dd811cb376ae831f7deaa26abf3fc7c4e1b
Toavina A <toavinahajaina@gmail.com>
2024-06-19 14:20:20 +0300

    WAC-83 add disclaimer to terms and conditions page (#30)
    * WAC 83 -
    
    * WAC 83 -
    
    ---------
    
    Co-authored-by: Jonathan Berry <jonathan.s.berry@gmail.com>

--- ckanext/who_afro/blueprints/__init__.py
+++ ckanext/who_afro/blueprints/__init__.py
@@ -1,5 +1,6 @@
 import ckanext.who_afro.blueprints.insights as insights
+import ckanext.who_afro.blueprints.terms as terms
 
 
 def get_blueprints():
-    return [insights.blueprint]
+    return [insights.blueprint, terms.blueprint]
