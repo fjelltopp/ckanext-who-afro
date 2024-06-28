e7030e9f87ea4e54aa0143de1f5ece9f4b158a70
toavina <toavinahajaina@gmail.com>
2024-06-28 16:22:03 +0300

    WAC 74 - Blueprint to emulate ckan/dashboard/user_activity

--- ckanext/who_afro/blueprints/__init__.py
+++ ckanext/who_afro/blueprints/__init__.py
@@ -1,7 +1,7 @@
 import ckanext.who_afro.blueprints.insights as insights
 import ckanext.who_afro.blueprints.terms as terms
-import ckanext.who_afro.blueprints.userflow as userflow
+import ckanext.who_afro.blueprints.dashboard_extension as dashboard_extension
 
 
 def get_blueprints():
-    return [insights.blueprint, terms.blueprint, userflow.blueprint]
+    return [insights.blueprint, terms.blueprint, dashboard_extension.blueprint]
--- ckanext/who_afro/blueprints/dashboard_extension.py
+++ ckanext/who_afro/blueprints/dashboard_extension.py
@@ -6,10 +6,11 @@ from ckanext.activity.views import dashboard
 log = logging.getLogger(__name__)
 
 blueprint = Blueprint(
-    "userflow", __name__, url_prefix="/dashboard"
+    "dashboard_extension", __name__, url_prefix="/dashboard"
 )
 
 
-@blueprint.get("/", endpoint="index")
+# Returns dashboard but with a different request.path
+@blueprint.get("/user_activity", endpoint="user_activity")
 def dashboard_index():
     return dashboard()
