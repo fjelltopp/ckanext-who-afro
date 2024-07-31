aae3938710b2dfa1a4e2cd4d69e5ff948abc7855
Ntwali B. <ntwali.bashige@gmail.com>
2024-07-31 15:17:59 +0100

    WAC-112: [WIP] Small improvements to the homepage redesign (#39)
    * WAC-112: [WIP] Small improvements
    
    - Made sure the focus underline for each menu item aligns properly with the hero section.
    - Aligned the icons and text in the “Find and share critical health data, fast” section in the hero section.
    - Changed the “Most viewed/downloaded” subsection title to “Featured data” and subsequently removed the “Featured data” section.
    
    * WAC-112: [WIP] Fixed padding in featured section.
    
    * WAC-112 Add data sources stub page
    
    * WAC-112 Fix main nav links
    
    * WAC-112 Fix home page links
    
    ---------
    
    Co-authored-by: Jonathan S Berry <jonathan.s.berry@gmail.com>

--- ckanext/who_afro/blueprints/__init__.py
+++ ckanext/who_afro/blueprints/__init__.py
@@ -1,7 +1,7 @@
 import ckanext.who_afro.blueprints.insights as insights
 import ckanext.who_afro.blueprints.countries as countries
 import ckanext.who_afro.blueprints.terms as terms
-
+import ckanext.who_afro.blueprints.sources as sources
 
 def get_blueprints():
-    return [insights.blueprint, countries.blueprint, terms.blueprint]
+    return [insights.blueprint, countries.blueprint, terms.blueprint, sources.blueprint]
