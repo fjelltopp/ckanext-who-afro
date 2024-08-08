a52cf169387f3b496fbb0afd3a1215bd361640a1
Jonathan S Berry <jonathan.s.berry@gmail.com>
2024-08-08 16:00:47 +0100

    WAC-116 Overview blueprint

--- ckanext/who_afro/blueprints/__init__.py
+++ ckanext/who_afro/blueprints/__init__.py
@@ -2,6 +2,14 @@ import ckanext.who_afro.blueprints.insights as insights
 import ckanext.who_afro.blueprints.countries as countries
 import ckanext.who_afro.blueprints.terms as terms
 import ckanext.who_afro.blueprints.sources as sources
+import ckanext.who_afro.blueprints.overview as overview
+
 
 def get_blueprints():
-    return [insights.blueprint, countries.blueprint, terms.blueprint, sources.blueprint]
+    return [
+        insights.blueprint,
+        countries.blueprint,
+        terms.blueprint,
+        sources.blueprint,
+        overview.blueprint
+    ]
