cc0617a21f7ab6f3eb3364b3c5123842c03f01fc
Jonathan Berry <jonathan.s.berry@gmail.com>
2024-08-13 11:53:35 +0100

    WAC-116 Indicator dataset dashboards (#42)
    * WAC-116 Helper to determine if there is an overview page for a dataset
    
    * WAC-116 Overview blueprint
    
    * WAC-116 Jinja2 and CSS changes to reformat dataset read pages.
    
    * WAC-116 Self review of code
    
    * wip: potentially working but I can't test it
    
    * feat: include JS and CSS assets
    
    * WAC-116 Cleanup bugs
    
    * WAC-85 Debug get_indicator_name helper
    
    * WAC-85 Point dashboard to local instance
    
    * WAC-85 Add Access and Use details to overview page
    
    ---------
    
    Co-authored-by: Chas Nelson <github@chasnelson.co.uk>

---
+++ ckanext/who_afro/blueprints/overview.py
@@ -0,0 +1,36 @@
+import logging
+from ckan.plugins import toolkit
+from flask import Blueprint
+
+log = logging.getLogger(__name__)
+
+blueprint = Blueprint(
+    "overview", __name__, url_prefix="/overview"
+)
+
+
+@blueprint.get("/<dataset_id>")
+def overview(dataset_id=None):
+    data_dict = {'id': dataset_id, 'include_tracking': True}
+
+    try:
+        pkg_dict = toolkit.get_action('package_show')({}, data_dict)
+    except (toolkit.ObjectNotFound, toolkit.NotAuthorized):
+        return toolkit.abort(
+            404,
+            toolkit._('Dataset not found or you have no permission to view it')
+        )
+
+    if not toolkit.h.dataset_has_overview(pkg_dict):
+        return toolkit.abort(
+            404,
+            toolkit._('Dataset does not have an overview page')
+        )
+
+    return toolkit.render(
+            'package/overview.html', {
+                'dataset_type': pkg_dict['type'],
+                'pkg_dict': pkg_dict,
+                'pkg': pkg_dict,
+            }
+        )
