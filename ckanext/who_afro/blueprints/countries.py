cc94a6272eb1d0116d5b9e231282ef5f6f77180b
Jonathan S Berry <jonathan.s.berry@gmail.com>
2024-08-15 10:21:07 +0100

    WAC-128 404 abort when country data not found

--- ckanext/who_afro/blueprints/countries.py
+++ ckanext/who_afro/blueprints/countries.py
@@ -18,7 +18,7 @@ def featured_countries():
     return toolkit.render("countries/index.html")
 
 
-def _load_country_data(country_id: str) -> dict:
+def _load_country_data() -> dict:
     country_data_url = toolkit.config.get('country_data_url')
     if not country_data_url:
         raise toolkit.ObjectNotFound("No country data file url specified")
@@ -30,13 +30,18 @@ def _load_country_data(country_id: str) -> dict:
         else:
             msg = f"Country data must be a local file - urls not yet supported {country_data_url}"
             raise Exception(msg)
+            # TODO: Below currently times out - because there's only one thread?
+            # timeout = toolkit.config.get('ckan.requests.timeout')
+            # response = requests.get(country_data_url, timeout=timeout)
+            # reader = csv.DictReader(response.raw)
+            # country_data = {row['iso2_code']: row for row in reader}
     except requests.RequestException as e:
         msg = "Couldn't get the country data file {}: {}".format(country_data_url, e)
         raise Exception(msg)
     except ValueError as e:
         msg = "Couldn't parse the country data file {}: {}".format(country_data_url, e)
         raise Exception(msg)
-    return country_data.get(country_id, {})
+    return country_data
 
 
 def _extract_src(html_string, default_val):
@@ -47,7 +52,14 @@ def _extract_src(html_string, default_val):
 
 @blueprint.get("/<country_id>")
 def country(country_id=None):
-    country_data = _load_country_data(country_id)
+    country_data = _load_country_data()
+    try:
+        country_data = country_data[country_id]
+    except KeyError:
+        return toolkit.abort(
+            404,
+            toolkit._(f'Country data not found for {country_id}')
+        )
     default_val = "Unfound"
     template_vars = {
         'iso2_code': country_data.get('iso2_code', default_val),
