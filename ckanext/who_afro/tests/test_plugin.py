29a88e2ba1de00220c4d674c478a868715cd72f7
Ntwali B <ntwali.bashige@gmail.com>
2024-08-28 16:02:00 +0100

    WAC-138: [WIP] Make sure that the PowerBI links correspond to the correct countries.

--- ckanext/who_afro/tests/test_plugin.py
+++ ckanext/who_afro/tests/test_plugin.py
@@ -105,43 +105,275 @@ class TestWHOAFROPluginBlueprints:
             "WHO global data health repository with health-related statistics from all member states"
         )
     
-    @pytest.mark.parametrize('country', [
-        "Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cabo Verde",
-        "Central African Republic", "Cameroon", "Congo", "Cote d'Ivoire", "Democratic Republic of Congo",
-        "Eritrea", "Ethiopia", "Gabon", "Ghana", "Guinea", "Gambia", "Equatorial Guinea", "Guinea-Bissau",
-        "Kenya", "Comoros", "Liberia", "Lesotho", "Madagascar", "Mali", "Mauritania", "Mauritius", "Malawi",
-        "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda", "Seychelles", "Sierra Leone", "Senegal",
-        "South Sudan", "Sao Tome and Principe", "Eswatini", "Chad", "Togo", "United Republic of Tanzania",
-        "Uganda", "South Africa", "Zambia", "Zimbabwe"
-    ])
-    def test_featured_countries_blueprint(self, app, country):
-        res = app.get('/countries/')
-        assert helpers.body_contains(
-            res,
-            country
-        )
+    # @pytest.mark.parametrize('country', [
+    #     "Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cabo Verde",
+    #     "Central African Republic", "Cameroon", "Congo", "Cote d'Ivoire", "Democratic Republic of Congo",
+    #     "Eritrea", "Ethiopia", "Gabon", "Ghana", "Guinea", "Gambia", "Equatorial Guinea", "Guinea-Bissau",
+    #     "Kenya", "Comoros", "Liberia", "Lesotho", "Madagascar", "Mali", "Mauritania", "Mauritius", "Malawi",
+    #     "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda", "Seychelles", "Sierra Leone", "Senegal",
+    #     "South Sudan", "Sao Tome and Principe", "Eswatini", "Chad", "Togo", "United Republic of Tanzania",
+    #     "Uganda", "South Africa", "Zambia", "Zimbabwe"
+    # ])
+    # def test_featured_countries_blueprint(self, app, country):
+    #     res = app.get('/countries/')
+    #     assert helpers.body_contains(
+    #         res,
+    #         country
+    #     )
     
     @pytest.mark.parametrize('country_data', [
-        ("DZ", "Algeria"), ("AO", "Angola"), ("BJ", "Benin"), ("BW", "Botswana"),
-        ("BF", "Burkina Faso"), ("BI", "Burundi"), ("CV", "Cabo Verde"),
-        ("CF", "Central African Republic"), ("CM", "Cameroon"), ("CG", "Congo"),
-        # It's a bit complicated to compare countries which names contain accented characters
-        # ("CI", "Côte d'Ivoire"),
-        ("CD", "Democratic Republic of the Congo"),
-        ("ER", "Eritrea"), ("ET", "Ethiopia"), ("GA", "Gabon"), ("GH", "Ghana"),
-        ("GN", "Guinea"), ("GM", "Gambia"), ("GQ", "Equatorial Guinea"),
-        ("GW", "Guinea-Bissau"), ("KE", "Kenya"), ("KM", "Comoros"), ("LR", "Liberia"),
-        ("LS", "Lesotho"), ("MG", "Madagascar"), ("ML", "Mali"), ("MR", "Mauritania"),
-        ("MU", "Mauritius"), ("MW", "Malawi"), ("MZ", "Mozambique"), ("NA", "Namibia"),
-        ("NE", "Niger"), ("NG", "Nigeria"), ("RW", "Rwanda"), ("SC", "Seychelles"),
-        ("SL", "Sierra Leone"), ("SN", "Senegal"), ("SS", "South Sudan"),
-        ("ST", "Sao Tome and Principe"), ("SZ", "Eswatini"), ("TD", "Chad"), ("TG", "Togo"),
-        ("TZ", "United Republic of Tanzania"), ("UG", "Uganda"), ("ZA", "South Africa"),
-        ("ZM", "Zambia"), ("ZW", "Zimbabwe")
+        ("DZ", "Algeria", {
+            "uhc": "https://app.powerbi.com/view?r=eyJrIjoiYTQ5NjE1YmMtZmZiOC00NGYzLTkwYzMtMDczYjUzMGMwNzdmIiwidCI6ImY2MTBjMGI3LWJkMjQtNGIzOS04MTBiLTNkYzI4MGFmYjU5MCIsImMiOjh9",
+            "hse": "https://app.powerbi.com/view?r=eyJrIjoiNjNhMmYwYzEtODVmMC00OWQzLTkwZGItZjk2MDkxODAyZTBiIiwidCI6ImY2MTBjMGI3LWJkMjQtNGIzOS04MTBiLTNkYzI4MGFmYjU5MCIsImMiOjh9",
+            "hpop": "https://app.powerbi.com/view?r=eyJrIjoiYmUyMmE3ZTgtOWE5YS00M2U2LThkYTMtNzMzNjI3ZTEyNmUxIiwidCI6ImY2MTBjMGI3LWJkMjQtNGIzOS04MTBiLTNkYzI4MGFmYjU5MCIsImMiOjh9"
+        }),
+        ("AO", "Angola", {
+            "uhc": "https://app.powerbi.com/view?r=eyJrIjoiNzBmNWJhMzUtMDBjZC00MzUxLWJjNTMtNzFjZTVhYjJmZTk3IiwidCI6ImY2MTBjMGI3LWJkMjQtNGIzOS04MTBiLTNkYzI4MGFmYjU5MCIsImMiOjh9",
+            "hse": "https://app.powerbi.com/view?r=eyJrIjoiOTA1MDNiNzItNmNjMC00MTM2LWI0YTEtNjk1MDZkOTYwNGI3IiwidCI6ImY2MTBjMGI3LWJkMjQtNGIzOS04MTBiLTNkYzI4MGFmYjU5MCIsImMiOjh9",
+            "hpop": "https://app.powerbi.com/view?r=eyJrIjoiMzNlZWI1ZGYtOTRkMy00ZWE4LWEwMDgtZTZhMjU4MzA5OTI4IiwidCI6ImY2MTBjMGI3LWJkMjQtNGIzOS04MTBiLTNkYzI4MGFmYjU5MCIsImMiOjh9"
+        }),
+        # ("BJ", "Benin", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("BW", "Botswana", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("BF", "Burkina Faso", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("BI", "Burundi", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("CV", "Cabo Verde", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("CF", "Central African Republic", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("CM", "Cameroon", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("CG", "Congo", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # # It's a bit complicated to compare countries which names contain accented characters
+        # # ("CI", "Côte d'Ivoire", {
+        # #     "uhc": "",
+        # #     "hse": "",
+        # #     "hpop": ""
+        # # }),
+        # ("CD", "Democratic Republic of the Congo", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("ER", "Eritrea", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("ET", "Ethiopia", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("GA", "Gabon", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("GH", "Ghana", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("GN", "Guinea", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("GM", "Gambia", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("GQ", "Equatorial Guinea", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("GW", "Guinea-Bissau", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("KE", "Kenya", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("KM", "Comoros", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("LR", "Liberia", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("LS", "Lesotho", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("MG", "Madagascar", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("ML", "Mali", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("MR", "Mauritania", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("MU", "Mauritius", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("MW", "Malawi", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("MZ", "Mozambique", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("NA", "Namibia", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("NE", "Niger", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("NG", "Nigeria", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("RW", "Rwanda", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("SC", "Seychelles", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("SL", "Sierra Leone", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("SN", "Senegal", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("SS", "South Sudan", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("ST", "Sao Tome and Principe", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("SZ", "Eswatini", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("TD", "Chad", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("TG", "Togo", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("TZ", "United Republic of Tanzania", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("UG", "Uganda", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("ZA", "South Africa", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("ZM", "Zambia", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # }),
+        # ("ZW", "Zimbabwe", {
+        #     "uhc": "",
+        #     "hse": "",
+        #     "hpop": ""
+        # })
     ])
     def test_country_blueprint(self, app, country_data):
         res = app.get('/countries/%s' % country_data[0])
         assert helpers.body_contains(
             res,
             country_data[1]
+        )
+        assert helpers.body_contains(
+            res,
+            country_data[2]["uhc"]
+        )
+        assert helpers.body_contains(
+            res,
+            country_data[2]["hse"]
+        )
+        assert helpers.body_contains(
+            res,
+            country_data[2]["hpop"]
         )
